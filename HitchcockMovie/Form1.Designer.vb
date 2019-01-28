2f974203e8a690451de73d4645dd9b87ab8ae7a1
Unknown <rhodricusack@gmail.com>
2019-01-28 15:22:01 +0000

    Initial commit

---
+++ HitchcockMovie/Form1.Designer.vb
@@ -0,0 +1,83 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.AxWindowsMediaPlayer1 = New AxWMPLib.AxWindowsMediaPlayer()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblCountDown = New System.Windows.Forms.Label()
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'AxWindowsMediaPlayer1
+        '
+        Me.AxWindowsMediaPlayer1.Enabled = True
+        Me.AxWindowsMediaPlayer1.Location = New System.Drawing.Point(12, 12)
+        Me.AxWindowsMediaPlayer1.Name = "AxWindowsMediaPlayer1"
+        Me.AxWindowsMediaPlayer1.OcxState = CType(resources.GetObject("AxWindowsMediaPlayer1.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxWindowsMediaPlayer1.Size = New System.Drawing.Size(1063, 855)
+        Me.AxWindowsMediaPlayer1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(169, 404)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(1417, 125)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Please wait a few moments"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblCountDown
+        '
+        Me.lblCountDown.AutoSize = True
+        Me.lblCountDown.Font = New System.Drawing.Font("Arial", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCountDown.Location = New System.Drawing.Point(701, 587)
+        Me.lblCountDown.Name = "lblCountDown"
+        Me.lblCountDown.Size = New System.Drawing.Size(278, 125)
+        Me.lblCountDown.TabIndex = 2
+        Me.lblCountDown.Text = "XXX"
+        Me.lblCountDown.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(11.0!, 24.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1972, 1024)
+        Me.Controls.Add(Me.lblCountDown)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.AxWindowsMediaPlayer1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents AxWindowsMediaPlayer1 As AxWMPLib.AxWindowsMediaPlayer
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblCountDown As Label
+End Class
---
+++ HitchcockMovie/Form1.vb
@@ -0,0 +1,41 @@
+﻿Imports System.IO
+Imports System.Reflection
+Public Class Form1
+    Dim intPulsesReceived = 0
+    Const intNumDummies = 3
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Me.WindowState = FormWindowState.Maximized
+        AxWindowsMediaPlayer1.Visible = False
+        lblCountDown.Text = (intNumDummies - intPulsesReceived).ToString
+    End Sub
+
+
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        If e.KeyCode = Keys.S Or e.KeyCode = Keys.D5 Then
+            intPulsesReceived = intPulsesReceived + 1
+            lblCountDown.Text = (intNumDummies - intPulsesReceived).ToString
+            If intNumDummies = intPulsesReceived Then
+                lblCountDown.Visible = False
+                Label1.Visible = False
+                AxWindowsMediaPlayer1.Visible = True
+                '                AxWindowsMediaPlayer1.windowlessVideo = True
+                AxWindowsMediaPlayer1.settings.autoStart = True
+                AxWindowsMediaPlayer1.URL = "bbyd_v3.mp4"
+            End If
+        End If
+    End Sub
+
+    Private Sub AxWindowsMediaPlayer1_StatusChange(sender As Object, e As EventArgs) Handles AxWindowsMediaPlayer1.StatusChange
+        If AxWindowsMediaPlayer1.status = 3 Then
+            AxWindowsMediaPlayer1.fullScreen = True
+        End If
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
+
+
